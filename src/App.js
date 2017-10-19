0e27818deeb749bdb6da96601f4fca357f50ae7a
jfalxa <jfalxa.sta@gmail.com>
2017-10-19 12:34:34 +0200

    Show a disk that follows the mouse

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { css } from 'react-emotion'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+const app = css`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`
 
-      </div>
-    );
-  }
+const indicator = css`
+    position: absolute;
+    width: 50px;
+    height: 50px;
+    background-color: #000;
+    border-radius: 25px;
+`
+
+function Indicator( { position } )
+{
+    const style =
+    {
+        top: position[1] - 25,
+        left: position[0] - 25
+    }
+
+    return <i className={ indicator } style={ style } />
 }
 
-export default App;
+class App extends Component
+{
+    state =
+    {
+        mousePosition: [ 0, 0 ]
+    }
+
+    componentDidMount()
+    {
+        this.startListening();
+    }
+
+    move = ( x, y ) =>
+    {
+        this.setState( { mousePosition: [ x, y ] } )
+    }
+
+    startListening = () =>
+    {
+        document.addEventListener( 'mousemove', e => this.move( e.clientX, e.clientY ) )
+    }
+
+    stopListening = () =>
+    {
+        document.removeEventListener( 'mousemove' );
+    }
+
+    render()
+    {
+        return (
+            <div className={app}>
+                <Indicator position={ this.state.mousePosition } />
+            </div>
+        )
+    }
+}
+
+export default App
