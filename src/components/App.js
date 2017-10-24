56729222ab536a80dc1551894b733b82a552df89
jfalxa <jfalxa.sta@gmail.com>
2017-10-24 16:24:40 +0200

    Block user selection on stage

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
 import React from 'react'
+import styled from 'react-emotion'
 import { Route } from 'react-router-dom'
 import Navigation from './Navigation'
 import EditStage from './EditStage'
 import MoveStage from './MoveStage'
 
 
+const AppContainer = styled( 'div' )`
+    user-select: none;
+`
+
+
 function App()
 {
     return (
 
-        <div>
+        <AppContainer>
             <Route exact path="/" component={ EditStage } />
             <Route path="/move" component={ MoveStage } />
 
             <Navigation />
-        </div>
+        </AppContainer>
 
     )
 }
