38896eb82eaeeb3e1ea2ce5a667ffe98a7023c88
jfalxa <jfalxa.sta@gmail.com>
2017-10-25 01:47:31 +0200

    Fixed bad behaviour for stage dragging when zoom is active

--- src/components/withPanZoomControls.js
+++ src/components/withPanZoomControls.js
@@ -4,10 +4,12 @@ import compose from 'lodash/flowRight'
 import withMovement from './withMovement'
 import { move, subtract, scale } from '../utils/geometry'
 
+
 function computeZoom( zoom, modifier )
 {
-    return Math.max( 0.1, Math.min( zoom + modifier/100, 3 ) )
+    return Math.max( 0.1, Math.min( zoom + modifier/300, 3 ) )
 }
+
 
 function withPanZoomControls( Component )
 {
@@ -45,9 +47,12 @@ function withPanZoomControls( Component )
             const { pan:prevPan, zoom:prevZoom } = this.state
 
             const position = [e.clientX, e.clientY]
+            const mouseDelta = subtract( delta, this._delta )
 
-            const deltaX = e.deltaX || this._delta[0] - delta[0] || 0
-            const deltaY = e.deltaY || this._delta[1] - delta[1] || 0
+            const [deltaX, deltaY] = scale( [
+                e.deltaX / 3 || mouseDelta[0] || 0,
+                e.deltaY / 3 || mouseDelta[1] || 0
+            ], 1/prevZoom )
 
             this._delta = delta
 
